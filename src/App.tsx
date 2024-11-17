123a038a622f294bc24e6ccc93ddfe19fff1dd65
SEJ <jink6111@gmail.com>
2024-11-17 18:14:21 +0900

    feat: [QZC-ROUTES] build init routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { routes } from './routes';
+
+const router = createBrowserRouter(routes());
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/routes/index.ts
@@ -0,0 +1,5 @@
+import { RouteObject } from 'react-router-dom';
+
+export function routes(): RouteObject[] {
+  return [];
+}
