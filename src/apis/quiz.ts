af1dce57a64179b8103895457b3dfca5a51825c1
SEJ <jink6111@gmail.com>
2024-11-22 16:56:09 +0900

    feat: [FUNCTION_home] add request api data
    refactor: [FUNCTION_home] modify font-size Typography in HomeContainer

--- src/apis/quiz.ts
+++ src/apis/quiz.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
  * 퀴즈 api 요청 함수
  * @see https://opentdb.com/api_config.php
  */
-export const getQuiz = async (): Promise<RES_QUIZ | undefined> => {
+export const getQuizess = async (): Promise<RES_QUIZ | undefined> => {
   const response = await axios.get(
     'https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple'
   );
--- src/containers/HomeContainer.tsx
+++ src/containers/HomeContainer.tsx
@@ -1,18 +1,46 @@
+import { useNavigate } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+
 import Button from '../components/Button';
 import ContentsWrapper from '../components/ContentsWrapper';
 import Typography from '../components/Typography';
+import { getQuizess } from '../apis/quiz';
 
 /**
  * 홈페이지 로직 컨테이너
  */
 export default function HomeContainer() {
+  const navigate = useNavigate();
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['quiz'],
+    queryFn: getQuizess,
+    staleTime: 0,
+    gcTime: 0,
+  });
+
+  const handleClick = () => {
+    if (!data) {
+      alert('퀴즈 데이터가 없습니다');
+      return;
+    }
+
+    sessionStorage.setItem('quizess', JSON.stringify(data));
+    navigate('/0');
+  };
+
+  const isDisabled = isLoading || isError;
   return (
     <ContentsWrapper className='gap-4'>
-      <Typography>
-        <Typography element='strong'>Hello, </Typography>
+      <Typography className='text-lg'>
+        <Typography className='text-xl' element='strong'>
+          Hello,{' '}
+        </Typography>
         Are you ready?
       </Typography>
-      <Button>Go</Button>
+      <Button onClick={handleClick} disabled={isDisabled}>
+        Go
+      </Button>
     </ContentsWrapper>
   );
 }
