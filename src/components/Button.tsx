0803cad67a388ceb513d71af006d7cf2619fd91d
SEJ <jink6111@gmail.com>
2024-11-22 16:57:34 +0900

    feat: [QZC-PUB] add disabled props in Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,20 +3,22 @@ import { twMerge } from 'tailwind-merge';
 interface Props {
   className?: string;
   children?: React.ReactNode;
+  disabled?: boolean;
   onClick?: () => void;
 }
 /**
  * 버튼 컴포넌트
  * @property {string} className tailwind class
  * @property {React.ReactNode} children
+ * @property {boolean} disabled default: false
  * @property {() => void} onClick 콜백 onClick
  */
-export default function Button({ className, children, onClick }: Props) {
+export default function Button({ className, children, disabled = false, onClick }: Props) {
   const basicClassName = 'px-2 py-1 border border-black rounded-md text-sm';
   const mergeClassName = twMerge(basicClassName, className);
 
   return (
-    <button className={mergeClassName} onClick={onClick}>
+    <button className={mergeClassName} onClick={onClick} disabled={disabled}>
       {children}
     </button>
   );
