8f45619b3caef56f04648808dd348cb807c04803
SEJ <jink6111@gmail.com>
2024-11-22 14:52:07 +0900

    feat: [QZC-PUB] publish home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,18 @@
+import Button from '../components/Button';
+import ContentsWrapper from '../components/ContentsWrapper';
+import Typography from '../components/Typography';
+
 /**
  * 홈페이지
  */
 export default function HomePage() {
-  return <div>Home</div>;
+  return (
+    <ContentsWrapper className='gap-4'>
+      <Typography>
+        <Typography element='strong'>Hello, </Typography>
+        Are you ready?
+      </Typography>
+      <Button>Go</Button>
+    </ContentsWrapper>
+  );
 }
